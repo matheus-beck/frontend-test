8896826ff33fc9434beadd81da75a50b7a4fe95b
Matheus de Alencar Beck <matheus.a.beck@ericssoninovacao.com.br>
2020-10-19 09:24:18 -0300

    Updates

---
+++ precache-manifest.4c9904a524cac855e09c312db987fd9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033826eac4cc351d0e93dd2d7bd5e0c8",
+    "url": "/frontend-test/index.html"
+  },
+  {
+    "revision": "e2e3a5080eb3fc319bf9",
+    "url": "/frontend-test/static/js/2.91ebe70a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/frontend-test/static/js/2.91ebe70a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d87e7143046ddc09dc47",
+    "url": "/frontend-test/static/js/main.3da88061.chunk.js"
+  },
+  {
+    "revision": "7f43fe872a7a4b4cafb8",
+    "url": "/frontend-test/static/js/runtime-main.afc04f91.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/frontend-test/static/media/background.3c9a1ce5.svg"
+  }
+]);
