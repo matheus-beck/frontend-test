721603eeeb080be136529fd931bb54f17851b2cc
Matheus Beck <matheus.alencarbeck@gmail.com>
2021-06-14 12:01:24 -0300

    Update precache-manifest.b052b774cf02980752f472101943ccc5.js

--- precache-manifest.b052b774cf02980752f472101943ccc5.js
+++ precache-manifest.b052b774cf02980752f472101943ccc5.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "02e63e8075d02d069d122151619591ff",
-    "url": "/frontend-test/index.html"
+    "url": "/react-events-app/index.html"
   },
   {
     "revision": "e2e3a5080eb3fc319bf9",
-    "url": "/frontend-test/static/js/2.91ebe70a.chunk.js"
+    "url": "/react-events-app/static/js/2.91ebe70a.chunk.js"
   },
   {
     "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
-    "url": "/frontend-test/static/js/2.91ebe70a.chunk.js.LICENSE.txt"
+    "url": "/react-events-app/static/js/2.91ebe70a.chunk.js.LICENSE.txt"
   },
   {
     "revision": "af762435c9c0030b0ad3",
-    "url": "/frontend-test/static/js/main.5a429c08.chunk.js"
+    "url": "/react-events-app/static/js/main.5a429c08.chunk.js"
   },
   {
     "revision": "7f43fe872a7a4b4cafb8",
-    "url": "/frontend-test/static/js/runtime-main.afc04f91.js"
+    "url": "/react-events-app/static/js/runtime-main.afc04f91.js"
   },
   {
     "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
-    "url": "/frontend-test/static/media/background.3c9a1ce5.svg"
+    "url": "/react-events-app/static/media/background.3c9a1ce5.svg"
   }
 ]);
